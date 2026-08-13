45d6b18830626b05c2af23bc6c58fcc3315eb957
Narayanan Chandran <cnarayan12ster@gmail.com>
2026-08-13 10:54:40 +0000

    update action

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { Octokit } from '@octokit/rest';
-import * as Checks from './namespaces/Checks';
+
 
 async function run() {
-  // try {
-  //   // const vid = core.getInput('vid');
-  //   // const vkey = core.getInput('vkey');
-  //   const decisionMode = core.getInput('decision_mode');
-  //   const sourceRepoName = core.getInput('source_repository');
-  //   const [owner, repo] = sourceRepoName.split('/');
-  //   const token = core.getInput('token');
-    
-  
-   
-   
-    
+   try {
+        const token = core.getInput("github_token");
 
+        const owner = core.getInput("repository_owner");
 
-    
+        const repo = core.getInput("repository_name");
 
-  //   core.info(`Fetched Veracode config from ${owner}/${repo}`);
-    
-  // } catch (error) {
-  //   core.setFailed(`Action failed: ${error instanceof Error ? error.message : String(error)}`);
-  // }
+        const name = core.getInput("check_run_name");
+
+        const headSha = core.getInput("head_sha");
+
+        const repository = core.getInput("repository");
+
+        const runId = core.getInput("run_id");
+
+        const detailsUrl = `https://github.com/${repository}/actions/runs/${runId}`;
+
+        const octokit = github.getOctokit(token);
+
+        const response = await octokit.rest.checks.create({
+            owner,
+            repo,
+            name,
+            head_sha: headSha,
+            details_url: detailsUrl
+        });
+
+        const checkRun = response.data;
+
+        core.setOutput("check_run_id", checkRun.id);
+        core.setOutput("check_run_url", checkRun.html_url);
+
+        core.info(`Check Run created successfully`);
+        core.info(`ID: ${checkRun.id}`);
+        core.info(`URL: ${checkRun.html_url}`);
+    } catch (error: any) {
+        core.setFailed(`Failed to create check run: ${error.message}`);
+    }
   console.log("Hello world! This is a test of the GitHub Action.");
 }
 
