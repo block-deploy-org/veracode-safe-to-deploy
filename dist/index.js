6daea4c2323f81db051b85f86c03162d9cb27526
Narayanan Chandran <cnarayan12ster@gmail.com>
2026-08-14 13:50:46 +0000

    update actions

--- dist/index.js
+++ dist/index.js
@@ -31866,22 +31866,31 @@ async function run() {
         const token = core.getInput("github_token");
         const owner = core.getInput("repository_owner");
         const repo = core.getInput("repository_name");
-        const name = core.getInput("check_run_name");
         const headSha = core.getInput("head_sha");
         const repository = core.getInput("repository");
-        const artifact_name = core.getInput("artifact_name");
-        const check_run_id = core.getInput("run_id");
-        const detailsUrl = `https://github.com/${repository}/actions/runs/${check_run_id}`;
+        const artifacts_list = core.getInput("artifacts_list");
         const octokit = github.getOctokit(token);
-        const response = await octokit.request('GET /repos/{owner}/{repo}/check-runs/{check_run_id}', {
+        const commits = await octokit.request(`GET /repos/{owner}/{repo}/commits/{headSha}`);
+        const check_run_id = commits.check_runs?.[0]?.id;
+        console.log(`Check Run ID: ${check_run_id}`);
+        const checkRunObj = await octokit.request(`GET /repos/{owner}/{repo}/check-runs/{check_run_id}`);
+        console.log(`Check Run Object: ${JSON.stringify(checkRunObj)}`);
+        checkRunObj.status = "completed";
+        checkRunObj.conclusion = "success";
+        checkRunObj.output = {
+            title: "Veracode Safe to Deploy Check",
+            summary: `Artifacts List: ${artifacts_list}`,
+            text: `Repository: ${repository}\nArtifacts List: ${artifacts_list}`
+        };
+        const checkRun = await octokit.request(`PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}`, {
             owner,
             repo,
             check_run_id,
             headers: {
                 'X-GitHub-Api-Version': '2026-03-10'
-            }
+            },
+            ...checkRunObj
         });
-        const checkRun = response.data;
         core.setOutput("check_run_id", checkRun.id);
         core.setOutput("check_run_url", checkRun.html_url);
         core.info(`Check Run created successfully`);
@@ -31889,9 +31898,8 @@ async function run() {
         core.info(`URL: ${checkRun.html_url}`);
     }
     catch (error) {
-        core.setFailed(`Failed to create check run: ${error.message}`);
+        core.setFailed("Error in Pipeline: " + error.message);
     }
-    console.log("Hello world! This is a test of the GitHub Action.");
 }
 run();
 
