f94c52dea8a4c467911272722995756f4085bc01
Narayanan Chandran <cnarayan12ster@gmail.com>
2026-08-14 14:22:07 +0000

    update action

--- dist/index.js
+++ dist/index.js
@@ -31881,7 +31881,7 @@ async function run() {
                 'X-GitHub-Api-Version': '2026-03-10'
             }
         });
-        const branchSha = branchObj.commit?.sha;
+        const branchSha = branchObj.response.commit?.sha;
         console.log(`Branch SHA: ${branchSha}`);
         const commits = await octokit.request(`GET /repos/{owner}/{repo}/commits/{branchSha}/check-runs`, { owner,
             repo,
